c34a6e52a3c9314443ebedce8e4c48e4d9e6b7e3
Nikitin Konstantin <nikitinkonstantin32@gmail.com>
2017-01-11 10:33:57 +0300

    minificator css and js

--- app/js/common.js
+++ app/js/common.js
@@ -1,19 +1,32 @@
-$(function(){
-     $(window).scroll(function () {
-        
+$(function () {
+    /*
+    *   Buton scroll top
+    */
+    $(window).scroll(function () {
+
         win = $(window).scrollTop();
-        
+
         if (win < 500) {
             $(".btn-top").hide();
-        }
-        else {
+        } else {
             $(".btn-top").show();
         }
     });
 
-    $(".btn-top a").on('click',function(){
-		 $('html, body').animate({
-          scrollTop: $($(this).data("href")).offset().top-159
-      }, 500);
-	});
+    $(".btn-top a").on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).data("href")).offset().top - 159
+        }, 500);
+    });
+
+    /*
+        * Flex slider
+    */
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 210,
+        itemMargin: 5
+    });
+    
 })
